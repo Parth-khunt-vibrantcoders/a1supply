fdaac92f2aadf33029ffd3b8a2867e300e9df526
parth <parth2.bvminfotech@gmail.com>
2023-02-13 10:03:33 +0530

    change admin setup

--- resources/views/backend/includes/body_footer.blade.php
+++ resources/views/backend/includes/body_footer.blade.php
@@ -5,7 +5,7 @@
         <!--begin::Copyright-->
         <div class="text-dark order-2 order-md-1">
             {{-- <span class="text-muted font-weight-bold mr-2"></span> --}}
-            <a href="{{get_system_setting_detail()->footer_link}}" target="_blank" class="text-dark-75 text-hover-primary"><?php echo date("Y");?> &copy; {{get_system_setting_detail()->footer_text}}</a>
+
         </div>
         <!--end::Copyright-->
         <!--begin::Nav-->
